b7dde7bf4d85698b03805b92cabf2ec119c197bb
waiga <hyang9387@gmail.com>
2022-03-20 14:57:41 +0900

    calculate sum average

--- CLangTest/main.c
+++ CLangTest/main.c
@@ -7,9 +7,24 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    printf("Git Commit");
+//int main(int argc, const char * argv[]) {
+//    // insert code here...
+//    printf("Hello, World!\n");
+//    printf("Git Commit");
+//    return 0;
+//}
+
+main() {
+    int n, i, t, sum = 0;
+    printf("number of integer?");
+    scanf("%d", &n);
+    for(i=1; i<=n; i++) {
+        printf("%d integer?: ", i);
+        scanf("%d", &t);
+        sum = sum + t;
+        
+    }
+    printf("sum: %d\n", sum);
+    printf("average: %f\n", (float)sum/n);
     return 0;
 }
