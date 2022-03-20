d5982595ebcb336632cffe1e9ac81b34f2c0db10
waiga <hyang9387@gmail.com>
2022-03-20 12:21:15 +0900

    Initial Commit

---
+++ CLangTest/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CLangTest
+//
+//  Created by 김미향 on 2022/03/20.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
