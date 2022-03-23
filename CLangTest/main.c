4117f6d2bf20cca45721a58de1954642b4a30d2d
waiga <hyang9387@gmail.com>
2022-03-23 10:10:17 +0900

    Condition Statement

--- CLangTest/main.c
+++ CLangTest/main.c
@@ -9,9 +9,12 @@
 
 
 main() {
-    float a;
-    char b;
-    char c[10];
-    scanf("%f %c %s", &a, &b, &c);
-    printf("%f %c %s", a, b, c);
+    int money, remain, book = 15000;
+    printf("책의 가격은 15000원입니다. \n");
+    printf("당신이 가진 용돈은 얼마인가요? : ");
+    scanf("%d", &money);
+    if (money >= book) {
+        remain = money - book;
+        printf("책을 구입하였습니다. 이제 남은 용돈은 %d 입니다.\n", remain);
+    }
 }
