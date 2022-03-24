d1c2bc9dff92924a7e84ef6176aa6cd64ebc6244
waiga <hyang9387@gmail.com>
2022-03-24 16:38:53 +0900

    array

--- CLangTest/main.c
+++ CLangTest/main.c
@@ -8,14 +8,18 @@
 #include <stdio.h>
 
 main() {
-    int num;
-    while (1) {
-        printf("숫자를 입력하세요 : ");
-        scanf("%d", &num);
-        if (num < 0) break;
-        if (num == 0) continue;
-        if (num % 2 == 1) printf("홀수\n");
-        else printf("짝수\n");
+    int A[4][2], s, i, j;
+    for(i=0; i<4; i++) {
+        printf("%d번의 영어, 수학 성적을 입력하시오. :", i+1);
+        for(j=0; j<2; j++)
+            scanf("%d", &A[i][j]);
+    }
+    for(i=0; i<4; i++) {
+        s=0;
+        printf("%d번 총 점수 : ", i+1);
+        for(j=0; j<2; j++)
+            s += A[i][j];
+        printf("%d\n", s);
     }
 }
 
